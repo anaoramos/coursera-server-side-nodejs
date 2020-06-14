4976687de067659e9013503f65e2c46968ec7f9e
anaoramos <anaforamos@gmail.com>
2020-06-14 20:52:06 +0100

    Basic Authentication

--- app.js
+++ app.js
@@ -30,7 +30,40 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+
+
+function auth(req, res, next){
+    console.log(req.headers);
+
+    var authHeader = req.headers.authorization;
+
+    if (!authHeader){
+        var err = new Error('You are not authenticated!');
+
+        res.setHeader('WWW-Authenticate','Basic');
+        err.status = 401;
+        return next(err);
+    }
+    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':'); //split into 2 parts: basic and encoded string
+    
+    var username = auth[0];
+    var password = auth[1];
+
+    if (username === 'admin' && password == 'password'){
+      next(); //se for aute. passa para o prox middleware
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+
+      res.setHeader('WWW-Authenticate','Basic');
+      err.status = 401;
+      return next(err);
+    }
+  }
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, 'public'))); //enables to serve static data 
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
