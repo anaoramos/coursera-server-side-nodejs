00e488818d88038182b3b4f24a640835bee3e809
anaoramos <anaforamos@gmail.com>
2020-06-15 19:24:01 +0100

    cookies

--- app.js
+++ app.js
@@ -29,37 +29,51 @@ app.set('view engine', 'jade');
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-app.use(cookieParser());
+app.use(cookieParser('12345-67890-09876-54321'));
 
 
-function auth(req, res, next){
-    console.log(req.headers);
-
+function auth(req, res, next) {
+  console.log(req.signedCookies);
+  if (!req.signedCookies.user) {
+    //measn that the user is not authenticate
     var authHeader = req.headers.authorization;
 
-    if (!authHeader){
-        var err = new Error('You are not authenticated!');
+    if (!authHeader) {
+      var err = new Error('You are not authenticated!');
 
-        res.setHeader('WWW-Authenticate','Basic');
-        err.status = 401;
-        return next(err);
+      res.setHeader('WWW-Authenticate', 'Basic');
+      err.status = 401;
+      return next(err);
     }
-    var auth = new Buffer(authHeader.split(' ')[1], 'base64').toString().split(':'); //split into 2 parts: basic and encoded string
-    
+    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); //split into 2 parts: basic and encoded string
+
     var username = auth[0];
     var password = auth[1];
 
-    if (username === 'admin' && password == 'password'){
+    if (username === 'admin' && password == 'password') {
+      res.cookie('user', 'admin', { signed: true });
       next(); //se for aute. passa para o prox middleware
     }
     else {
       var err = new Error('You are not authenticated!');
 
-      res.setHeader('WWW-Authenticate','Basic');
+      res.setHeader('WWW-Authenticate', 'Basic');
       err.status = 401;
       return next(err);
     }
   }
+  else {
+    if (req.signedCookies.user === 'admin') {
+      next();
+    }
+    else {
+      var err = new Error('You are not authenticated!');
+      err.status = 401;
+      return next(err);
+    }
+  }
+
+}
 
 app.use(auth);
 
@@ -72,12 +86,12 @@ app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
