ef3efdb129d2b117e0e3b60706b4cd8ff2eb57a4
anaoramos <anaforamos@gmail.com>
2020-06-17 08:59:29 +0100

    Express Session Part 2

--- app.js
+++ app.js
@@ -41,44 +41,26 @@ app.use(session({
   store: new FileStore()
 }));
 
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
 function auth(req, res, next) {
   console.log(req.session);
 
   if (!req.session.user) {
     //measn that the user is not authenticate
-    var authHeader = req.headers.authorization;
-
-    if (!authHeader) {
       var err = new Error('You are not authenticated!');
-
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
+      err.status = 403;
       return next(err);
-    }
-    var auth = new Buffer.from(authHeader.split(' ')[1], 'base64').toString().split(':'); //split into 2 parts: basic and encoded string
-
-    var username = auth[0];
-    var password = auth[1];
-
-    if (username === 'admin' && password == 'password') {
-      req.session.user = 'admin';
-      next(); //se for aute. passa para o prox middleware
-    }
-    else {
-      var err = new Error('You are not authenticated!');
-
-      res.setHeader('WWW-Authenticate', 'Basic');
-      err.status = 401;
-      return next(err);
-    }
   }
   else {
-    if (req.session.user === 'admin') {
+    if (req.session.user === 'authenticated') {
       next();
     }
     else {
       var err = new Error('You are not authenticated!');
-      err.status = 401;
+      err.status = 403;
       return next(err);
     }
   }
@@ -89,8 +71,6 @@ app.use(auth);
 
 app.use(express.static(path.join(__dirname, 'public'))); //enables to serve static data 
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
 app.use('/dishes', dishRouter);
 app.use('/promotions', promoRouter);
 app.use('/leaders', leaderRouter);
